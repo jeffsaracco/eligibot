6fb395227b5782bff3b01cdbd0831c3f2b10e028
Jeff Saracco <jeffsaracco@gmail.com>
2013-04-28 20:21:26 -0400

    formatting:

--- eligibot.rb
+++ eligibot.rb
@@ -1,4 +1,4 @@
 require 'chatterbot/dsl'
 search("'eligable -rt'") do |tweet|
-    reply "#{tweet_user(tweet)} I believe you mean 'eligible'", tweet
+  reply "#{tweet_user(tweet)} I believe you mean 'eligible'", tweet
 end
