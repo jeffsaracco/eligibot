67308a4247a7c8257fa8b0aa153deb643b920971
Jeff Saracco <jeffsaracco@gmail.com>
2013-06-25 01:18:03 +0000

    Add victory tweet

--- eligibot.rb
+++ eligibot.rb
@@ -2,3 +2,4 @@ require 'chatterbot/dsl'
 search("'eligable -rt'") do |tweet|
   reply "#{tweet_user(tweet)} I believe you mean 'eligible'", tweet
 end
+tweet "Just schooled another round of Eligible Tweeters!"
